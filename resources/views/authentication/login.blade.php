d9c11d05177c6d8413dd74ed3b503d0c5ce62cb1
LQT-PC\LQT <thai.ly@beesightsoft.com>
2017-08-29 18:40:38 +0700

    Login page

---
+++ resources/views/authentication/login.blade.php
@@ -0,0 +1,41 @@
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h3 class="panel-title">
+                        Login
+                    </h3>
+                </div>
+
+                <div class="panel-body">
+                    <form action="/login" method="POST">
+                        {{ csrf_field() }}
+
+                        <div class="form-group">
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
+                                <input type="email" name="email" class="form-control" placeholder="Email" required>
+                            </div>
+                        </div>
+
+
+                        <div class="form-group">
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+                                <input type="password" name="password" class="form-control" placeholder="Password">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <input type="submit" name="submit" value="Login" class="btn btn-success pull-right">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,5 @@ Route::get('/register', 'RegistrationController@register');
 
 Route::post('/register', 'RegistrationController@postRegister');
 
+Route::get('/login', 'LoginController@login');
+Route::post('/login', 'LoginController@postLogin');
